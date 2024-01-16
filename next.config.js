620c5b2d11e048df4da2446048722a297c12f6a3
Donvine <donvinemugendi@gmail.com>
2024-01-17 01:19:25 +0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+export const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
