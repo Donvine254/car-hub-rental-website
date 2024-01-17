5f713313f55b98c8623429ca8975c9c460297d32
Donvine <donvinemugendi@gmail.com>
2024-01-17 04:55:19 +0300

    updated validations

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
       <div className="p-6 flex xsm:flex-col items-center gap-2 bg-blue-100 bg-opacity-40 rounded-md">
         <Link
           href="/login"
-          className="inline-flex items-center justify-center text-xl font-medium border disabled:pointer-events-none disabled:bg-gray-100 disabled:text-black h-10 w-20 px-4 py-2 xsm:w-full bg-gray-200 hover:bg-gray-300 text-black rounded-md">
+          className="inline-flex items-center justify-center text-xl font-medium border disabled:pointer-events-none disabled:bg-gray-100 disabled:text-black h-8 w-20 px-4 py-1 xsm:w-full bg-gray-200 hover:bg-gray-300 text-black rounded-md">
           Login
         </Link>
         <Link
           href="/register"
-          className="inline-flex items-center justify-center text-xl font-medium border border-blue-500 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-black px-4 py-2 xsm:w-full  hover:bg-blue-600  rounded-md text-black hover:text-white h-10 w-20">
+          className="inline-flex items-center justify-center text-xl font-medium border border-blue-500 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-black px-4 py-1 xsm:w-full  hover:bg-blue-600  rounded-md text-black hover:text-white h-8 w-20">
           Register
         </Link>
       </div>
