9db1612416ff1086bf4f8fa86e26533a0529da83
Donvine <donvinemugendi@gmail.com>
2024-01-19 02:37:57 +0300

    update navigation bar

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,9 @@
-
 import Navbar from "@/components/ui/navbar";
+import NavigationMenu from "@/components/ui/navigationmenu";
 export default function Home() {
   return (
     <main>
-      <Navbar />
+      <NavigationMenu />
       <section className="" id="hero-page">
         <h1 className="text-2xl font-bold text-center my-10">
           Welcome to Car Hub
