999096014d1744f65e8ca52db551a03ca8d55ca8
Donvine <donvinemugendi@gmail.com>
2024-01-18 23:48:29 +0300

    updated navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -41,100 +41,101 @@ const components: { title: string; href: string }[] = [
   },
   {
     title: "Logout",
-    href: "/me/logout",
+    href: "/api/logout",
   },
 ];
 
 export default function Navbar(props: Props) {
   return (
-    <NavigationMenu className="my-2 text-lg">
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link href="/" legacyBehavior passHref>
-            <Image
-              alt="CarHub Logo"
-              width={118}
-              height={18}
-              src="/logo.svg"
-              className="object-contain"
-            />
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="text-2xl">
-            Cars
-          </NavigationMenuTrigger>
-          <NavigationMenuContent className="min-w-[200px] rounded-md">
-            <ul className="flex flex-col gap-3 w-full ">
-              <ListItem href="/cars?model=saloon" title="Saloons"></ListItem>
-              <ListItem href="/cars?model=suv" title="SUVs"></ListItem>
-              <ListItem href="/cars?model=pickup" title="Pickups"></ListItem>
-              <ListItem href="/cars?model=van" title="Vans"></ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="text-2xl">
-            Locations
-          </NavigationMenuTrigger>
-          <NavigationMenuContent className="min-w-[200px] rounded-md">
-            <ul className="flex flex-col gap-3 w-full ">
-              <ListItem
-                href="/locations?location=nairobi"
-                title="Nairobi"></ListItem>
-              <ListItem
-                href="/locations?location=eldoret"
-                title="Eldoret"></ListItem>
-              <ListItem
-                href="/locations?location=Kisumu"
-                title="Kisumu"></ListItem>
-              <ListItem
-                href="/locations?location=Mombasa"
-                title="Mombasa"></ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/booking" legacyBehavior passHref>
-            <NavigationMenuLink
-              className={`${navigationMenuTriggerStyle()} text-xl`}>
-              Booking
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="text-xl">
-            My Account
-          </NavigationMenuTrigger>
-          <NavigationMenuContent className="bg-blue-100 bg-opacity-10">
-            <ul className="flex flex-col gap-3 min-w-[200px]">
-              {components.map((component) => (
+    <nav className="flex flex-wrap items-center justify-between gap-5 m-2">
+      <Link href="/" legacyBehavior passHref>
+        <Image
+          alt="CarHub Logo"
+          width={118}
+          height={18}
+          src="/logo.svg"
+          className="object-contain"
+        />
+      </Link>
+
+      <NavigationMenu className="text-lg">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="text-2xl">
+              Cars
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                <ListItem href="/cars?model=saloon" title="Saloons"></ListItem>
+                <ListItem href="/cars?model=suv" title="SUVs"></ListItem>
+                <ListItem href="/cars?model=pickup" title="Pickups"></ListItem>
+                <ListItem href="/cars?model=van" title="Vans"></ListItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="text-2xl">
+              Locations
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
                 <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}></ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/pricing" legacyBehavior passHref>
-            <NavigationMenuLink
-              className={`${navigationMenuTriggerStyle()} text-xl`}>
-              Pricing
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/Login" legacyBehavior passHref>
-            <NavigationMenuLink
-              className={`${navigationMenuTriggerStyle()} text-xl`}>
-              Login
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+                  href="/locations?location=nairobi"
+                  title="Nairobi"></ListItem>
+                <ListItem
+                  href="/locations?location=eldoret"
+                  title="Eldoret"></ListItem>
+                <ListItem
+                  href="/locations?location=Kisumu"
+                  title="Kisumu"></ListItem>
+                <ListItem
+                  href="/locations?location=Mombasa"
+                  title="Mombasa"></ListItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="text-xl">
+              My Account
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                {components.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.href}></ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/booking" legacyBehavior passHref>
+              <NavigationMenuLink
+                className={`${navigationMenuTriggerStyle()} text-xl`}>
+                Booking
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/pricing" legacyBehavior passHref>
+              <NavigationMenuLink
+                className={`${navigationMenuTriggerStyle()} text-xl`}>
+                Pricing
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/login" prefetch legacyBehavior passHref>
+              <NavigationMenuLink
+                className={`${navigationMenuTriggerStyle()} text-xl`}>
+                Login
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </nav>
   );
 }
 
@@ -148,7 +149,7 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground ",
             className
           )}
           {...props}>
