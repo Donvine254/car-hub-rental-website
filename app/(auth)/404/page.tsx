bb7a8104a6155097e6a0a99b9cb4950848658775
Donvine <donvinemugendi@gmail.com>
2024-01-23 01:40:24 +0300

    add 404 page

---
+++ app/(auth)/404/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NotFound from "../[...not-found]/page";
+type Props = {};
+
+export default function page({}: Props) {
+  return (
+    <>
+      <NotFound />
+    </>
+  );
+}
