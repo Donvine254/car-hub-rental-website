08ce1ba63ece915252fb3b2ac79db976e458e8d6
Donvine <donvinemugendi@gmail.com>
2024-03-19 01:57:32 +0300

    update navigation menu

--- components/ui/navigationmenu.tsx
+++ components/ui/navigationmenu.tsx
@@ -146,6 +146,13 @@ export default function NavigationMenu({ variant }: Props) {
                         Pickup
                       </Link>
                     </li>
+                    <li>
+                      <Link
+                        href="/cars"
+                        className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">
+                        View All
+                      </Link>
+                    </li>
                   </ul>
                 </div>
               </li>
