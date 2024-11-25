b05a3cf89949bb81b602abe9eee2b12b558dc2e8
Donvine <donvinemugendi@gmail.com>
2024-11-25 22:15:09 +0300

    add terms and conditions page

---
+++ app/(dashboard)/terms/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Car Hub - Terms and Conditions",
+  description:
+    "Car Hub is a car rental service that provides customers with ease access to high-end, high-performance and affordable rental vehicles",
+};
+type Props = {};
+
+export default function Terms({}: Props) {
+  return <div>rental terms and conditions</div>;
+}
