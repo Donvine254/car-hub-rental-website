47250d4be3294e0cbd491b7a8c11f0ddea301fe6
Donvine <donvinemugendi@gmail.com>
2024-11-28 03:06:52 +0300

    add sidenav component

--- app/(dashboard)/me/page.tsx
+++ app/(dashboard)/me/page.tsx
@@ -9,7 +9,7 @@ export default async function Profile({}: Props) {
   const cookieStore = cookies();
   const supabase = createServerComponentClient({ cookies: () => cookieStore });
   const { data, error } = await supabase.auth.getUser();
-
+  console.log(data);
   if (error?.status === 401 || !data) {
     redirect("/login?post_login_redirect_url=me");
   }
---
+++ components/ui/sidenav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function SideNav({}: Props) {
+  return (
+    <div>sidenav</div>
+  )
+}
