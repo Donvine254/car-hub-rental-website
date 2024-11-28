41e9f0b3af9af801987d4e53446d8420ad948d7f
Donvine <donvinemugendi@gmail.com>
2024-11-28 03:32:59 +0300

    add metadata to profile page

--- app/(dashboard)/me/profile/page.tsx
+++ app/(dashboard)/me/profile/page.tsx
@@ -3,7 +3,12 @@ import { Stats } from "@/components/ui/stats";
 import { RecentOrders } from "@/components/ui/recentorders";
 import { Favorites } from "@/components/ui/favorites";
 import SideNav from "@/components/ui/sidenav";
-
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Car Hub - My Profile ",
+  description:
+    "Car Hub is a car rental service that provides customers with ease access to high-end, high-performance and affordable rental vehicles",
+};
 type Props = {};
 
 export default async function Profile({}: Props) {
