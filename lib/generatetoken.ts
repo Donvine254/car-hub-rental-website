ebc152af85f6208b8ea61c77e2049ef59268b630
Donvine <donvinemugendi@gmail.com>
2024-11-30 23:00:00 +0300

    update generate token route

--- lib/generatetoken.ts
+++ lib/generatetoken.ts
@@ -1,12 +1,11 @@
-// import * as jose from "jose";
-const jose = require("jose");
-const baseUrl = "http://localhost:3000/verify";
+import * as jose from "jose";
+const baseUrl = "https://carhubke.vercel.app/verify";
 const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
 const user = {
   email: "donvinemugendi@gmail.com",
   id: "2",
 };
-export async function generateToken(email, id) {
+export async function generateToken(email: string, id: string) {
   // Generate JWT token with a 1-day expiration time
   const token = await new jose.SignJWT({ userId: id, email: email })
     .setProtectedHeader({ alg: "HS256" })
