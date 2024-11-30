c08fdbc2ec07509fcb745ec7589e611faea880c4
Donvine <donvinemugendi@gmail.com>
2024-11-30 04:21:59 +0300

    update favorites

--- components/ui/favorites.tsx
+++ components/ui/favorites.tsx
@@ -20,7 +20,7 @@ export async function Favorites() {
             className="bg-white rounded-lg shadow-lg border overflow-hidden flex items-center flex-wrap p-4">
             {/* Car Image */}
             <Image
-              alt={car.model_name}
+              alt={car.modelName}
               src={car.image || "/vehicle-placeholder.png"}
               width={300}
               height={200}
@@ -31,7 +31,7 @@ export async function Favorites() {
 
             {/* Car Details */}
             <div className="flex-1 px-6 space-y-4">
-              <h3 className="text-xl font-bold">{car.model_name}</h3>
+              <h3 className="text-xl font-bold">{car.modelName}</h3>
               <div className="flex items-center justify-between gap-4 flex-wrap">
                 <div className="grid grid-cols-2 gap-4 lg:gap-x-8 text-sm">
                   <div>
@@ -45,17 +45,17 @@ export async function Favorites() {
                   <div>
                     <p className="font-medium">Fuel Consumption:</p>
                     <p className="text-muted-foreground">
-                      {car.fuel_consumption} L/100km
+                      {car.fuelConsumption} L/100km
                     </p>
                   </div>
                   <div>
                     <p className="font-medium">Transmission:</p>
-                    <p className="text-muted-foreground">{car.transmission}</p>
+                    <p className="text-muted-foreground">{car.transmissionType}</p>
                   </div>
                   <div>
                     <p className="font-medium">Body Type:</p>
                     <p className="text-muted-foreground capitalize">
-                      {car.body_type}
+                      {car.bodyType}
                     </p>
                   </div>
                   <div>
@@ -70,11 +70,11 @@ export async function Favorites() {
                       Daily rate from
                     </p>
                     <p className="text-2xl md:text-3xl my-1 font-bold text-green-600">
-                      ${car.price_per_day}
+                      ${car.pricePerDay}
                     </p>
                   </div>
                   <Link
-                    href={`/booking?car_model=${car.model_name}&price=${car.price_per_day}`}
+                    href={`/booking?id=${car.id}&car_model=${car.modelName}&price=${car.pricePerDay}`}
                     className="mt-4 bg-green-500 text-center text-white px-4 py-1 rounded-md hover:bg-green-600">
                     Rent Now
                   </Link>
