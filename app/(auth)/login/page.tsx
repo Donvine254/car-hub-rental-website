decb568f0bb098b5e35a2ed2d45c50500cd61604
Donvine <donvinemugendi@gmail.com>
2024-12-02 13:29:46 +0300

    update cloudflare turnstile

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -8,7 +8,7 @@ import { toast } from "sonner";
 import Link from "next/link";
 import axios from "axios";
 import TurnstileComponent from "@/components/ui/turnstile";
-import verifyTurnstileToken from "@/lib/helpers";
+import verifyTurnstileToken from "@/lib/actions/verifycaptcha";
 type Props = {};
 
 interface FormData {
@@ -194,7 +194,7 @@ export default function Login({}: Props) {
             <button
               className="inline-flex items-center justify-center text-xl font-medium border disabled:pointer-events-none disabled:bg-green-50 disabled:text-black  h-10 px-4 py-2 w-full bg-green-500 hover:bg-green-600 text-white rounded-md"
               type="submit"
-              disabled={loading}
+              disabled={loading || !token}
               title="login">
               {!loading ? (
                 "Login"
--- lib/helpers/index.ts
+++ lib/helpers/index.ts
@@ -1,25 +1,2 @@
 //helper functions should be defined here
 //Formatting data for UI, API wrappers.
-export default async function verifyTurnstileToken(token: string) {
-  const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
-
-  const formData = new FormData();
-  formData.append("secret", process.env.TURNSTILE_SECRET_KEY || "");
-  formData.append("response", token);
-
-  try {
-    const result = await fetch(url, {
-      body: formData,
-      method: "POST",
-    });
-
-    const outcome = await result.json();
-    if (outcome.success) {
-      return true;
-    }
-  } catch (err) {
-    console.error(err);
-    return false;
-  }
-  return false;
-}
