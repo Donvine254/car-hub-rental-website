d0dd06be0799103ebd2ba413b5aed9c3f5f9a23c
Donvine <donvinemugendi@gmail.com>
2024-12-02 12:12:10 +0300

    add function to verify turnstile token

--- components/ui/turnstile.tsx
+++ components/ui/turnstile.tsx
@@ -2,23 +2,18 @@ import React from "react";
 import Turnstile from "react-turnstile";
 
 interface TurnstileComponentProps {
-  onVerify: (token: string) => void; 
-  onExpire?: () => void; 
-  onError?: () => void; 
+  onVerify: (token: string) => void;
+  onExpire?: () => void;
+  onError?: () => void;
 }
 
-const siteKey = process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY; 
+const siteKey = process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY as string;
 
 const TurnstileComponent: React.FC<TurnstileComponentProps> = ({
   onVerify,
   onExpire,
   onError,
 }) => {
-  if (!siteKey) {
-    console.error("Turnstile siteKey is not set.");
-    return <div>Error: CAPTCHA siteKey is missing.</div>; // Handle missing siteKey gracefully
-  }
-
   return (
     <Turnstile
       sitekey={siteKey}
