71dbc7d420cb8f14ec6eafb0120161b54c8a1aa7
Donvine <donvinemugendi@gmail.com>
2024-12-04 00:55:30 +0300

    update login page loader

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -246,7 +246,7 @@ export default function Register({}: Props) {
             <button
               className="inline-flex items-center justify-center text-xl font-medium border disabled:pointer-events-none disabled:bg-green-50 disabled:text-black  h-10 px-4 py-2 w-full bg-green-500 hover:bg-green-600 text-white rounded-md"
               type="submit"
-              disabled={loading || !isDev ? !token : false}
+              disabled={loading}
               title="register">
               {!loading ? (
                 "Register"
