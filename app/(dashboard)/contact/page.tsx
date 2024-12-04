72ceeb815104d71958b315c67e66fbf3dcfa81d2
Donvine <donvinemugendi@gmail.com>
2024-12-04 03:20:18 +0300

    update contact page redirect to help page

--- app/(dashboard)/contact/page.tsx
+++ app/(dashboard)/contact/page.tsx
@@ -1,8 +1,7 @@
 import { redirect } from "next/navigation";
-import React from "react";
 
 type Props = {};
 
 export default function ContactPage({}: Props) {
-  return redirect("/me/profile");
+  return redirect("/help");
 }
