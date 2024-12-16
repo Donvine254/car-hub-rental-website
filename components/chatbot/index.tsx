0663059a2e08cd016ab0ea0493c845539ba16a7f
Donvine <donvinemugendi@gmail.com>
2024-12-16 16:36:32 +0300

    fix chatbox responsiveness on mobile devices

--- components/chatbot/index.tsx
+++ components/chatbot/index.tsx
@@ -2,12 +2,11 @@
 
 import { useState, useEffect } from "react";
 
-import Image from "next/image";
-import { Button } from "../ui/button";
 import { ChatBox } from "./chatbox";
-
-export const Chatbot = () => {
-  const [isOpen, setIsOpen] = useState(false);
+type Props = {
+  setIsOpen: (state:boolean) => void;
+};
+export const Chatbot = ({ setIsOpen }: Props) => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
@@ -19,27 +18,11 @@ export const Chatbot = () => {
     return () => window.removeEventListener("resize", checkMobile);
   }, []);
 
-  const toggleChat = () => setIsOpen(!isOpen);
-
   return (
     <>
-      <Button
-        onClick={toggleChat}
-        className="flex items-center justify-center md:justify-start  w-full md:w-fit bg-white text-green-600 px-6 py-2 rounded-md border hover:bg-gray-600 hover:text-white border-green-600">
-        <Image
-          src="https://res.cloudinary.com/dipkbpinx/image/upload/v1733141454/illustrations/ai-svgrepo-com_boip47.svg"
-          alt="AI logo"
-          height={24}
-          width={24}
-          priority
-        />
-        <p>Ask AI Support Agent</p>
-      </Button>
       <div
         className={`fixed ${isMobile ? "inset-0" : "bottom-4 right-4"} z-50`}>
-        {isOpen && (
-          <ChatBox isMobile={isMobile} onClose={() => setIsOpen(false)} />
-        )}
+        <ChatBox isMobile={isMobile} onClose={() => setIsOpen(false)} />
       </div>
     </>
   );
