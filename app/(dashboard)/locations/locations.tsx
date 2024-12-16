cbb40924f5242bdc259059faed6d176cc2ec87c2
Donvine <donvinemugendi@gmail.com>
2024-12-16 22:09:44 +0300

    add location page

---
+++ app/(dashboard)/locations/locations.tsx
@@ -0,0 +1,143 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import CarCard from "@/components/ui/car-card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Locations } from "@/constants";
+import { Car } from "@/lib/actions/car-actions/fetchCars";
+import { getSession } from "@/lib/actions/session";
+import { Clock, Mail, MapPinnedIcon, Phone } from "lucide-react";
+import { toast } from "sonner";
+
+type Props = {
+  Cars: Car[];
+};
+
+interface Location {
+  id: number;
+  name: string;
+  address: string;
+  phone: string;
+  mapUrl: string;
+}
+
+export default function LocationsPage({ Cars }: Props) {
+  const router = useRouter();
+  async function handleBooking(car: Car) {
+    const session = await getSession();
+    if (!session) {
+      toast.error("Login required to perform this action! ", {
+        position: "top-center",
+      });
+      setTimeout(() => {
+        router.push(
+          `/login?post_login_redirect_url=/booking?id=${car.id}&car_model=${car.modelName}&price=${car.pricePerDay}`
+        );
+      }, 1000);
+    } else {
+      router.push(
+        `/booking?id=${car.id}&car_model=${car.modelName}&price=${car.pricePerDay}`
+      );
+    }
+  }
+  return (
+    <div className="min-h-screen mx-2  md:w-3/4 md:mx-auto p-2 bg-[#f8f9fa] ">
+      <Tabs defaultValue={Locations[0].name} className="w-full">
+        <TabsList className="flex justify-start gap-2 md:gap-4 overflow-x-auto w-full border-b rounded-none h-auto p-0 bg-transparent">
+          {Locations.map((location) => (
+            <TabsTrigger
+              key={location.id}
+              value={location.name}
+              className="capitalize font-medium text-xl data-[state=active]:border-b-2 data-[state=active]:border-green-500 rounded-none px-6 ">
+              {location.name}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        {Locations.map((location) => (
+          <TabsContent key={location.id} value={location.name}>
+            <LocationCard location={location} />
+          </TabsContent>
+        ))}
+        <hr />
+        {Locations.map((location) => (
+          <TabsContent key={location.id} value={location.name}>
+            <div>
+              <h2 className="text-2xl xsm:text-lg font-semibold">
+                Available Vehicles in{" "}
+                <span className="capitalize">{location.name}</span>
+              </h2>
+              <section className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 py-2 ">
+                {Cars.filter((car) => car.location === location.name).length >
+                0 ? (
+                  Cars.filter((car) => car.location === location.name).map(
+                    (car) => (
+                      <div
+                        key={car.id}
+                        className="transition-shadow duration-300 hover:shadow-[0_4px_15px_rgba(0,0,0,0.2)]">
+                        <CarCard car={car} handleBooking={handleBooking} />
+                      </div>
+                    )
+                  )
+                ) : (
+                  <div className="text-center text-gray-500 col-span-full">
+                    No cars available for this location
+                  </div>
+                )}
+              </section>
+            </div>
+          </TabsContent>
+        ))}
+      </Tabs>
+    </div>
+  );
+}
+
+const LocationCard = ({ location }: { location: Location }) => {
+  return (
+    <div className="flex flex-col md:flex-row gap-5 py-2">
+      {/* first child */}
+      <div className="border shadow rounded-md bg-white py-6 px-4 space-y-4 flex-shrink">
+        <h2 className="text-2xl xsm:text-lg font-semibold mb-4 capitalize">
+          {location.name}
+          {""} Rental Office
+        </h2>
+        <div className="flex items-center space-x-2 mb-2">
+          <MapPinnedIcon fill="none" className="text-green-500 mr-2" />
+          <span>{location.address}</span>
+        </div>
+        <div className="flex items-center space-x-2 mb-4">
+          <Phone className="mr-2 fill-green-500 stroke-none" />
+          <span>{location.phone}</span>
+        </div>
+        <div className="flex items-center space-x-2 mb-4">
+          <Mail className="mr-2" fill="#22C55E" stroke="white" />
+          <a
+            href="mailto:info@carhubke.vercel.app"
+            className="text-blue-600 hover:underline">
+            info@carhubke.com
+          </a>
+        </div>
+        <div className="flex items-center space-x-2 mb-4">
+          <Clock fill="none" className="text-green-500 mr-2" />
+          <p>Mon - Fri 08.00 - 18.00</p>
+        </div>
+        <a
+          href="mailto:support@carbubke.vercel.app"
+          className="flex items-center xsm:justify-center gap-2 w-full xsm:w-full bg-white text-green-600 px-6 py-2 rounded-md border hover:bg-green-500 hover:text-white border-green-600 transition-colors">
+          <Mail /> Get in Touch
+        </a>
+      </div>
+      {/* second child */}
+      <div className="h-[300px] flex-1">
+        <iframe
+          src={location.mapUrl}
+          width="100%"
+          height="100%"
+          className="border rounded-md xsm:min-h-[350px]"
+          allowFullScreen
+          referrerPolicy="no-referrer-when-downgrade"
+          loading="lazy"></iframe>
+      </div>
+    </div>
+  );
+};
--- constants/index.tsx
+++ constants/index.tsx
@@ -56,4 +56,45 @@ export const testimonials = [
   },
 ];
 
-
+export const Locations = [
+  {
+    id: 1,
+    name: "nairobi",
+    address: "123 Kenyatta Avenue, Nairobi",
+    phone: "+254702018099",
+    mapUrl:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d255281.19891888283!2d36.70730744863283!3d-1.3031933999999908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1172d84d49a7%3A0xf7cf0254b297924c!2sNairobi%2C%20Kenya!5e0!3m2!1sen!2sus!4v1639763145256!5m2!1sen!2sus",
+  },
+  {
+    id: 2,
+    name: "thika",
+    address: "456 Thika Road, Thika",
+    phone: "+254702018099",
+    mapUrl:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63820.61595083051!2d37.02651204535237!3d-1.0384344133075592!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f4e5b27c66117%3A0xb6f8a7e126152c26!2sThika!5e0!3m2!1sen!2ske!4v1639763226558!5m2!1sen!2ske",
+  },
+  {
+    id: 3,
+    name: "eldoret",
+    address: "789 Uganda Road, Eldoret",
+    phone: "+254702018099",
+    mapUrl:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127632.76851130753!2d35.23105460329574!3d0.5142307076538971!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1781026982c3f1f7%3A0xaf17b4a6f3a3aea8!2sEldoret%2C%20Kenya!5e0!3m2!1sen!2sus!4v1639763291781!5m2!1sen!2sus",
+  },
+  {
+    id: 4,
+    name: "kisumu",
+    address: "101 Oginga Odinga Street, Kisumu",
+    phone: "+254702018099",
+    mapUrl:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127672.76851130753!2d34.23105460329574!3d-0.0942307076538971!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182aa437ad4ac81f%3A0x2c35690da1b0fc7c!2sKisumu%2C%20Kenya!5e0!3m2!1sen!2sus!4v1639763345781!5m2!1sen!2sus",
+  },
+  {
+    id: 5,
+    name: "mombasa",
+    address: "202 Moi Avenue, Mombasa",
+    phone: "+254702018099",
+    mapUrl:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127712.76851130753!2d39.23105460329574!3d-4.0342307076538971!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x184012e78ec02c7d%3A0xcb618bbc35d0db5a!2sMombasa%2C%20Kenya!5e0!3m2!1sen!2sus!4v1639763400781!5m2!1sen!2sus",
+  },
+];
