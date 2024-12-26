b42e422a37a166a10a2e17245b1c14979990b3c5
Donvine <donvinemugendi@gmail.com>
2024-12-26 21:58:58 +0300

    feat: enhance advanced search form animation for improved user experience

--- app/(dashboard)/marketplace/search-form.tsx
+++ app/(dashboard)/marketplace/search-form.tsx
@@ -80,7 +80,9 @@ export default function SearchForm() {
         {/* advanced search */}
         <div
           className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-x-4 gap-y-2 transition-all duration-300 ease-in-out overflow-hidden mt-2 ${
-            showAdvanced ? "opacity-100 max-h-[500px]" : "opacity-0 max-h-0"
+            showAdvanced
+              ? "opacity-100 max-h-[500px] delay-300"
+              : "opacity-0 max-h-0 delay-150 "
           }`}>
           <div className="md:hidden lg:block">
             <label className="block text-sm font-medium text-gray-700">
