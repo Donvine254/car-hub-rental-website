42ccd0e371ec172122f3fbc6e98741efcec72826
Donvine <donvinemugendi@gmail.com>
2024-12-26 19:53:13 +0300

    feat: optimize RandomImage component with useMemo and adjust image display timing

--- app/(auth)/login/random-image.tsx
+++ app/(auth)/login/random-image.tsx
@@ -1,14 +1,17 @@
 "use client";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 const RandomImage = () => {
-  const imageSources = [
-    "https://res.cloudinary.com/dipkbpinx/image/upload/v1734537690/cars/b2irjm7jub55tqtxaqya.png",
-    "https://res.cloudinary.com/dipkbpinx/image/upload/v1735229935/cars/mjt3l1eatzgqqndludxs.png",
-    "https://res.cloudinary.com/dipkbpinx/image/upload/v1706566320/cars/wi2vgjmhzqnsqn0h86cp.png",
-    "https://res.cloudinary.com/dipkbpinx/image/upload/v1704846148/cars/w7fluonkjjwx5ukwlyhm.png",
-  ];
+  const imageSources = useMemo(
+    () => [
+      "https://res.cloudinary.com/dipkbpinx/image/upload/v1734537690/cars/b2irjm7jub55tqtxaqya.png",
+      "https://res.cloudinary.com/dipkbpinx/image/upload/v1735229935/cars/mjt3l1eatzgqqndludxs.png",
+      "https://res.cloudinary.com/dipkbpinx/image/upload/v1706566320/cars/wi2vgjmhzqnsqn0h86cp.png",
+      "https://res.cloudinary.com/dipkbpinx/image/upload/v1704846148/cars/w7fluonkjjwx5ukwlyhm.png",
+    ],
+    []
+  );
 
   const [currentImage, setCurrentImage] = useState(imageSources[0]);
 
@@ -16,17 +19,17 @@ const RandomImage = () => {
     const interval = setInterval(() => {
       const randomIndex = Math.floor(Math.random() * imageSources.length);
       setCurrentImage(imageSources[randomIndex]);
-    }, 3000); // Change every 3 seconds
+    }, 5000);
 
-    return () => clearInterval(interval); // Cleanup on component unmount
-  }, []);
+    return () => clearInterval(interval);
+  }, [imageSources]);
 
   return (
     <Image
       src={currentImage}
       alt="Car rental illustration"
-      width={800}
-      height={450}
+      width={960}
+      height={540}
       className="my-auto"
       priority
     />
